dc35dd50d22892aabaf69fab87ba7a3bbef57f4a
shohanbhuiyan03 <shohanbhuiyan35@gmail.com>
2021-02-28 01:35:17 +0600

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+<b-container class="bv-example-row">
+  <b-row align-v="center">
+    <JobCard/>
+    <JobCard/>
+    <JobCard/>
+    <JobCard/>
+
+  </b-row>
+</b-container>
+</template>
+
+<script>
+// @ is an alias to /src
+import JobCard from '@/components/JobCard.vue'
+
+export default {
+  name: 'Home',
+  components: {
+     JobCard
+  
+  }
+}
+</script>
