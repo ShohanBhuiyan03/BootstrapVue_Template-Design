4d3560abefab4669366b14e46cb4a70359b1f5da
shohanbhuiyan03 <shohanbhuiyan35@gmail.com>
2021-02-28 15:54:06 +0600

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
 <b-container class="bv-example-row">
   <b-row align-v="center">
-    <JobCard/>
-    <JobCard/>
-    <JobCard/>
-    <JobCard/>
-
+    <JobCard v-for="job in displayJobs" :key="job.id" :name= "job.name" /> 
   </b-row>
+  <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      first-text="First"
+      prev-text="Prev"
+      next-text="Next"
+      last-text="Last"
+      @input="paginate(currentPage)"
+    ></b-pagination>
 </b-container>
 </template>
 
@@ -19,6 +25,36 @@ export default {
   components: {
      JobCard
   
-  }
+  },
+  mounted() {
+    this.fetchData();
+  },
+  data() {
+    return {
+      jobs: [],
+      displayJobs: [],
+      currentPage: 1,
+      rows: 1,
+      perPage: 3
+
+    }
+  },
+  methods: {
+    async fetchData() {
+      const res = await fetch("jobs.json");
+      const val = await res.json();
+      this.jobs = val;
+      this.displayJobs = val.slice(0,3);
+      this.rows = this.jobs.length;
+      console.log(val);
+      
+    },
+    paginate() {
+      const start =  (this.currentPage-1)*this.perPage;
+      this.displayJobs = this.jobs.slice(start,start+3);
+
+    }
+
+  },
 }
 </script>
